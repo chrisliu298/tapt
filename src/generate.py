6be2e393e25a9fd94bdb7d82993c9ae9ce5c0cfa
convexlifefunction <yuhaoliu298@gmail.com>
2020-07-18 20:06:01 -0700

    Add comments

--- src/generate.py
+++ src/generate.py
@@ -1,48 +1,41 @@
+# Standard
 import collections
-from dict2obj import Dict2Obj
+
+# Third party
 import numpy as np
-import torch
 import pandas as pd
+import torch
 import tqdm
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, AutoConfig, set_seed
+
+from dict2obj import Dict2Obj
+from transformers import GPT2LMHeadModel
+from transformers import GPT2Tokenizer
 
 
 class Generator:
-    def __init__(
-        self,
-        model_type,
-        model_name_or_path,
-        num_return_sequences=1,
-        length=512,
-        repetition_penalty=1.0,
-        temperature=1.0,
-        k=0,
-        p=0.9,
-        no_cuda=False,
-        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-        seed=42,
-    ):
-        self.model_type = model_type
-        self.model_name_or_path = model_name_or_path
-        self.num_return_sequences = num_return_sequences
-        self.length = length
-        self.repetition_penalty = repetition_penalty
-        self.temperature = temperature
-        self.k = k
-        self.p = p
-        self.no_cuda = no_cuda
+    """GPT-2 model generator.
+
+    Attributes:
+        device: A string indicating the use of GPU or CPU. 
+                torch.device("cuda" if torch.cuda.is_available() else "cpu")   
+    """
+
+    def __init__(self, device):
         self.device = device
-        self.seed = seed
         self.MAX_LENGTH = int(10000)
         self.start_token = "<|startoftext|>"
         self.sep_token = "<|sep|>"
         self.stop_token = "<|endoftext|>"
+        self.has_start_token = has_start_token
 
     def set_seed(self, seed):
+        """Sets random seed for numpy and torch"""
         np.random.seed(seed)
         torch.manual_seed(seed)
 
     def adjust_length_to_model(self, length, max_sequence_length):
+        # sourcery skip: merge-duplicate-blocks, remove-redundant-if
+        """Sets generation sequence length"""
         if length < 0 and max_sequence_length > 0:
             length = max_sequence_length
         elif 0 < max_sequence_length < length:
@@ -51,12 +44,28 @@ class Generator:
             length = self.MAX_LENGTH
         return length
 
-    def generate(self, tokenizer, model, prompt):
-        set_seed(self.seed)
-        self.length = self.adjust_length_to_model(
-            self.length, max_sequence_length=model.config.max_position_embeddings
+    def generate(
+        self,
+        tokenizer,
+        model,
+        prompt,
+        num_return_sequences=1,
+        length=256,
+        repetition_penalty=1.0,
+        temperature=1,
+        k=0,
+        p=0.9,
+        no_cuda=False,
+        device=self.device,
+        seed=42,
+        has_start_token=False,
+    ):
+        """Generates a sequence of words of specified length given an input prompt."""
+        self.set_seed(self.seed)
+        length = self.adjust_length_to_model(
+            length, max_sequence_length=model.config.max_position_embeddings
         )
-        prompt_text = self.start_token + " " + prompt
+        prompt_text = self.start_token + " " + prompt if has_start_token else prompt
         encoded_prompt = tokenizer.encode(
             prompt_text, add_special_tokens=False, return_tensors="pt"
         )
@@ -65,11 +74,11 @@ class Generator:
         input_ids = None if encoded_prompt.size()[-1] == 0 else encoded_prompt
         output_sequences = model.generate(
             input_ids=input_ids,
-            max_length=self.length + len(encoded_prompt[0]),
-            temperature=self.temperature,
-            top_k=self.k,
-            top_p=self.p,
-            repetition_penalty=self.repetition_penalty,
+            max_length=length + len(encoded_prompt[0]),
+            temperature=temperature,
+            top_k=k,
+            top_p=p,
+            repetition_penalty=repetition_penalty,
             do_sample=True,
             num_return_sequences=1,
         )
@@ -80,7 +89,9 @@ class Generator:
         generated_sequences = []
         for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
             generated_sequence = generated_sequence.tolist()
-            text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
+            text = tokenizer.decode(
+                generated_sequence, clean_up_tokenization_spaces=True
+            )
             text = text[: text.find(self.stop_token) if self.stop_token else None]
             total_sequence = (
                 prompt_text
